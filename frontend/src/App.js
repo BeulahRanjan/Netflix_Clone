91d26e5cc174f6526a69fe5008ee2bbaa0ac96b1
Beulah Ranjan <beulahranjan@gmail.com>
2026-02-06 16:47:51 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,52 @@
-
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
+import Slider from "./components/Slider";
 
 function App() {
+  const handleGetStarted = () => {
+    window.scrollBy({
+      top: 170,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="app-container">
+        <div className="top-content">
+          <img src={logo} alt="Netflix Logo" className="logo" />
+          <div className="buttons">
+            <button>English</button>
+            <button>Sign In</button>
+          </div>
+        </div>
+
+        <div className="content">
+          <h1>
+            Unlimited movies,<br /> shows, and more
+          </h1>
+          <h2>Starts at ₹149. Cancel at any time.</h2>
+          <p>
+            Ready to watch? Enter your email to create or restart your membership.
+          </p>
+
+          <div className="input-group">
+            <input type="email" placeholder="Email address" />
+            <button onClick={handleGetStarted}>
+              Get Started &gt;
+            </button>
+          </div>
+        </div>
+
+        {/* RED CURVE WITH BLUE LIGHT */}
+        
+      </div>
+
+      <div className="subapp-container">
+       <Slider />
+
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Slider.js
@@ -0,0 +1,103 @@
+import React from "react";
+import Slider from "react-slick";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Slider.css";
+import pic1 from "../assets/pic1.webp";
+import pic2 from "../assets/pic2.webp";
+import pic3 from "../assets/pic3.webp";
+import pic4 from "../assets/pic4.webp"; 
+import pic5 from "../assets/pic5.webp";
+import pic6 from "../assets/pic6.webp";
+import pic7 from "../assets/pic7.webp";
+import pic8 from "../assets/pic8.webp";
+import pic9 from "../assets/pic9.webp";
+import pic10 from "../assets/pic10.webp";
+
+/* ---------- Custom Arrows ---------- */
+
+function NextArrow(props) {
+  return (
+    <div className="arrow next"
+   
+      onClick={props.onClick}
+    >
+      <ChevronRightIcon />
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  return (
+    <div className="arrow prev"
+      onClick={props.onClick}
+    >
+      <ChevronLeftIcon />
+    </div>
+  );
+}
+
+/* ---------- Dummy Slider Data ---------- */
+
+const movies= [
+     { id: 1, img: pic1 },
+    { id: 2, img: pic2 },
+    { id: 3, img: pic3 },
+    { id: 4, img: pic4 },
+    { id: 5, img: pic5 },
+    { id: 6, img: pic6 },
+    { id: 7, img: pic7 },
+    { id: 8, img: pic8 },
+    { id: 9, img: pic9 },
+    { id: 10, img: pic10 },
+
+];
+
+/* ---------- Main Component ---------- */
+
+function SliderComponent() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 640,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
+
+  return (
+    <div >
+
+
+       <div className="slider-container">
+      <Slider {...settings}>
+        {movies.map((movie, index) => (
+          <div className="card" key={movie.id}>
+            <img src={movie.img} alt={`movie-${index}`} />
+            <p> {index + 1}</p>
+            <span className="number">{index + 1}</span>
+          </div>
+        ))}
+      </Slider>
+    </div>
+      
+    </div>
+  );
+}
+
+export default SliderComponent;
