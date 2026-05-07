3adb51ce5f8d5907874382faa3041fb65e8231a0
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-07 13:32:13 +0530

    feat(tmdb): add movie APIs for trending, details, trailers, and related data

---
+++ backend/controllers/movieController.js
@@ -0,0 +1,42 @@
+import { fetchFromTMDB } from "../services/tmdbService.js";
+
+export async function getTrendingMovies(req, res) {
+    try{
+        const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
+        const randomMovies = data.results[Math.floor(Math.random() * data.results?.length)];
+
+        res.json({ success:true, content: randomMovies });
+    }
+    catch(error){
+        res.status(500).json({ success:false, "Internal Server Error" });
+    }
+}
+
+export async function getMovieTrailers(req, res) {
+    const { id } = req.params;
+    try{
+        const data= await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+        res.json({ success:true, content: data.results });
+    }
+    catch(error){
+        if(error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+        res.status(500).json({ success:false, "Internal Server Error" });
+    }
+}
+
+export async function getMovieDetails(req, res) {
+    const { id } = req.params;
+    try{
+        const data= await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        res.status(200).json({ success:true, content:data});
+    }
+    catch(error){
+        if(error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+
+        res.status(500).json({ success:false, "Internal Server Error" });
+    }
+}
