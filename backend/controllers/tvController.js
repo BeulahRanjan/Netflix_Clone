93e46223f240a6b78a0fdc2e1c1b29a8172c60cb
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-08 11:05:06 +0530

    feat(tmdb): add TV APIs for trending, details, trailers, and related data

--- backend/controllers/tvController.js
+++ backend/controllers/tvController.js
@@ -25,3 +25,17 @@ export async function getTVTrailers(req, res) {
     res.json({success:false, message:"Internal Server Error"});
    }
 }
+
+export async function getTVDetails(req, res) {
+    const { id } =req.params;
+    try{
+        const data= await fetchFromTMDB(`https://api.themoviedb.org/3/tv/${id}?language=en-US`);
+        res.json({ success:true, content:data.results});
+    } 
+    catch(error){
+        if(error.message.includes(404)){
+            return res.status(404).send(null);
+        }
+        res.json({ success:false, messsage:"intenal server Error"});
+    }
+}
