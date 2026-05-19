0446e58b583d7c057a2ab4040ac4f79544b8079f
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-19 21:46:35 +0530

    feat(nginx): configure reverse proxy and load balancing for backend servers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import movieRoutes from "./routes/movieRoutes.js";
+import tvRoutes from "./routes/tvRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
 import { ENV_VARS } from "./config/envVars.js";
  import { connectDB } from "./config/db.js";
  import cookieParser from 'cookie-parser';
+ import { protectRoute } from "./middleware/protectRoute.js";
  import path, { dirname } from 'path';
 
  const app= express();
@@ -11,10 +16,19 @@ const _dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("api/v1/auth", authRoutes);
+app.use("api/v1/movie", protectRoute, movieRoutes);
+app.use("api/v1/tv", protectRoute, tvRoutes);
+app.use("api/v1/search", protectRoute, searchRoutes);
 
-app.get("/", (req, res) => {
-  res.send(`Response from server running on ${process.env.PORT}`);
-});
+if(ENV_VARS.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"/frontend/dist")));
+
+    app.get("*" , (req,res) =>{
+        res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+    })
+}
+
 
 app.listen(PORT, ()=>{
     console.log('Server is running at http://localhost:'+ PORT);
