75633376cf35e101ea1126f9f36b08c83228fee4
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-19 16:53:36 +0530

    feat(nginx): configure reverse proxy and load balancing for backend servers

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ import { ENV_VARS } from "./config/envVars.js";
  import path, { dirname } from 'path';
 
  const app= express();
-const PORT = ENV_VARS.PORT;
+const PORT = ENV_VARS.PORT || 5000;
 const _dirname=path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
 
+app.get("/", (req, res) => {
+  res.send(`Response from server running on ${process.env.PORT}`);
+});
 
 app.listen(PORT, ()=>{
     console.log('Server is running at http://localhost:'+ PORT);
