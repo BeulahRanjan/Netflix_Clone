5162bb5f64569c8b5629b0ac4386b13b2613b47a
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-20 19:33:33 +0530

    Setup frontend with Vite, Tailwind CSS, routing, and state management

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,21 +1,44 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    ecmaVersion: 2020,
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parserOptions: {
+        ecmaVersion:'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+       },
     },
+    settings:{ react: {version: '19.2'}},
+    plugins:{
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        }
+      ],
+      'react/no-unescaped-entities': 'off',
+    }
   },
-])
+]
