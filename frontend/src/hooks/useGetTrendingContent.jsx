480f9d127e4548657c2541a002651c7b7e58e454
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-22 18:58:35 +0530

    Implement authentication state management using Zustand

--- frontend/src/hooks/useGetTrendingContent.jsx
+++ frontend/src/hooks/useGetTrendingContent.jsx
@@ -2,4 +2,18 @@ import {useEffect, useState} from "react";
 import {useCOntentStore} from "../store/content.js";
 import axios from "axios";
 
-const
+const useGetTrendingContent = () => {
+    const [trendingContent, setTrendingContent] =useState(null);
+    const {contentType} = useCOntentStore();
+
+    useEffect(() => {
+        const getTrendingContent =async () =>{
+            const res =await axios.get(`/api/v1/${contentType}/trending`);
+            setTrendingContent(res.data.content);
+        };
+        getTrendingContent();
+    }, [contentType]);
+
+    return {trendingContent};
+    };
+export default useGetTrendingContent;
