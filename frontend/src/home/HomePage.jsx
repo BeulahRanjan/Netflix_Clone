234ccd1e4592ef17e727293fd91199b17370c5b9
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-26 19:01:53 +0530

    Implement authentication check flow using useEffect and Zustand

---
+++ frontend/src/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import HomeScreen from "../home/HomeScreen";
+import AuthScreen from "../home/AuthScreen";
+import {useAuthStore } from "../store/authUser";
+
+const HomePage = () => {
+    const {user} = useAuthStore();
+    return(
+        <div>
+            {user ? <HomeScreen /> : <AuthScreen />}
+        </div>
+    )
+}
+
+export default HomePage;
