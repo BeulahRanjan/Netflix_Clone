674b5bc0c65f1a242b083013d0610a0760349d25
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-05-31 21:50:22 +0530

    Add and update Navbar with UI and functionality improvements

--- frontend/src/home/HomePage.jsx
+++ frontend/src/home/HomePage.jsx
@@ -2,11 +2,12 @@ import HomeScreen from "../home/HomeScreen";
 import AuthScreen from "../home/AuthScreen";
 import {useAuthStore } from "../store/authUser";
 import Navbar from "../components/Navbar";
+import MovieSlider from "../components/MovieSlider";
 const HomePage = () => {
     const {user} = useAuthStore();
     return(
         <div>
-            {user ? <AuthScreen /> : <Navbar />}
+            {user ? <AuthScreen /> : <MovieSlider />}
         </div>
     )
 }
