bf78054918fab090d87822d6cbe519ae67f134fc
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-04 18:38:16 +0530

    Add and update HomeScreen Component with UI and functionality improvements

--- frontend/src/home/HomeScreen.jsx
+++ frontend/src/home/HomeScreen.jsx
@@ -60,7 +60,7 @@ function HomeScreen() {
 
         <div className='flex mt-8'>
           <Link 
-           to={`/watch/${trendingCOntent?.id}`}
+           to={`/watch/${trendingContent?.id}`}
            className='bg-white hover:bg-white/80 text-blacck font-bold py-2 px-4 rounded mr-4 flex items-center'>
             <Play className='size-6 mr-2 fill-black'/>
             Play
