d04d34ff2f59f687d5e0ea46e41174fb0087af04
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-05 23:46:26 +0530

    Add and update Login Page with UI and functionality improvements

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import {React,useState} from 'react'
 import {Link} from "react-router-dom";
-import Navbar from "../components/Navbar.jsx";
+import Navbar from "../../components/Navbar.jsx";
 import { Info, Play } from 'lucide-react';
-import useGetTrendingContent from "../hooks/useGetTrendingContent.jsx";
-import {MOVIE_CATEGORIES,ORIGINAL_IMG_BASE_URL, TV_CATEGORIES} from "../utils/constants.js";
-import { useContentStore } from '../store/content.js';
-import MovieSlider from "../components/MovieSlider.jsx";
+import useGetTrendingContent from "../../hooks/useGetTrendingContent.jsx";
+import {MOVIE_CATEGORIES,ORIGINAL_IMG_BASE_URL, TV_CATEGORIES} from "../../utils/constants.js";
+import { useContentStore } from '../../store/content.js';
+import MovieSlider from "../../components/MovieSlider.jsx";
 
 
 function HomeScreen() {
