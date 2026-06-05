c1a28dac4c64688d2c2c09eb5e39b5c686ccb164
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-06 00:30:24 +0530

    Add and update Login Page with UI and functionality improvements

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { useAuthStore } from '../store/authUser';
+
+function LoginPage(){
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const { login } = useAuthStore();
+}
