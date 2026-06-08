16659f48570ebb03f79b7e785847b70f32fa9310
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-08 22:18:01 +0530

    Add and update Login Page with UI and functionality improvements

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,13 +6,12 @@ function LoginPage(){
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const { login } = useAuthStore();
-}
+
 
 const handleSubmit = async(e) =>{
-    e.preventDefault()
+    e.preventDefault();
     login({email,password});
 }
-
 return(
     <div className='h-screen w-full hero-bg'>
         <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
@@ -20,6 +19,22 @@ return(
             <img src="/netflix-logo.png" alt="Netflix" className='w-52' />
             </Link>
         </header>
+
+        <div className='flex justify-center items-center mt-20 mx-3'>
+            <div className='w-full max-w-md p-8 space-y-6 bg-black/50 rounded-lg shadow-md'>
+            <h1 className='text-center text-white text-2xl font-bold mb-4'>Login</h1>
+            <form className='space-y-4' onSubmit={handleSubmit}>
+                <label htmlFor='email'  classname='text-sm font-medium text-gray-300 block'> Email</label>
+                <input type='email' 
+                 className='w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'
+                 placeholder='you@example.com'
+                 id='email'
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}/>
+            </form>
+            </div>
+        </div>
     </div>
-)
+)}
+
 export default LoginPage;
