8cb8a93228e4d1ba0f47d91be8611535a0655f0f
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-09 21:14:40 +0530

    Add and update Signup Page with UI and functionality improvements

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -14,6 +14,17 @@
             e.preventDefault();
             signup({email,username,password});
         }
+
+        return(
+            <div className='h-screen w-full hero-bg'>
+                <header className='max-w-6xl mx-auto flex items-center justify-between p-4'>
+                    <Link to={"/"}>
+                        <img src="/netflix-logo.png" alt="logo" className='w-52'/>
+                    </Link>
+                </header>
+                
+            </div>
+        )
     }
 
     export default SignupPage;
