041f393aae151bce465c920fe7205be4b177b2c2
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-10 23:11:44 +0530

    Add and update Not Found(404) Page with UI and functionality improvements

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -6,8 +6,11 @@ const NotFoundPage = () => {
         className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
         style={{backgroundImage: `url('/404.png')`}}>
             <header className='absolute top-0 left-0 p-4 bg-black w-full'>
-                
+                <Link to={"/"}>
+                <img src="/netflix-logo.png" alt="Netflix" className="h-8" />
+                </Link>
             </header>
+            
         </div>
     )
 }
