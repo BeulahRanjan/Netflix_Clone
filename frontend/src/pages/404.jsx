d14d6dae5bc540b12d305bb4107bd55b1191a8a8
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-10 23:14:15 +0530

    Add and update Not Found(404) Page with UI and functionality improvements

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -10,7 +10,12 @@ const NotFoundPage = () => {
                 <img src="/netflix-logo.png" alt="Netflix" className="h-8" />
                 </Link>
             </header>
-            
+            <main className='text-center error-page--content z-10'>
+                <h1 className='text-7xl font-semibold mb-4'>Lost your way?</h1>
+                <p className="mb-6 text-xl">Sorry, we can&pos;t find that page. You&pos;ll find losts to explore on the home page.</p>
+
+
+            </main>
         </div>
     )
 }
