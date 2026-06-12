07010d105f759d451471d62e2518c90e36b3314f
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-12 23:17:36 +0530

    Add and update Search History Page with UI and functionality improvements

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -56,8 +56,42 @@ const SearchHistoryPage = () => {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
+
+    return(
+        <div className="bg-black text-white min-h-screen">
+            <Navbar/>
+            <div className="max-w-6xl mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold mb-8">Search History</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {searchHistory?.map((entry)=>(
+                        <div key={entry.id} className="bg-gray-800 p-4 roundd flex items-start">
+                            <img
+                                 src={SMALL_IMG_BASE_URL + entry.image}
+                                 alt="History image"
+                                 className="size-16 rounded-full object-cover mr-4"/>
+                            <div className="flex flex-col">
+                                <span className="text-white text-lg">{entry.title}</span>
+                                <span className="text-gray-400 text-sm">{formatDate(entry.createdAt)}</span>
+                            </div>
+                            <span
+                                className={`py-1 px-3 min-w-20 text-center rounded-full text-sm ml-auto ${
+                                    entry.searchType ==="movie"
+                                    ? "bg-red-600": entry.searchType ==="tv"
+                                    ? "bg-blue-600":"bg-green-600"
+                                }`}>
+                                    {entry.searchType[0].toUpperCase()+entry.searchType.slice(1)}
+                                </span>
+                                <Trash
+                                    className="size-5 ml-4 cursor-pointer over:fill-red-600 hover:text-red-600"
+                                    onClick={() => handleDelete(entry)}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SearchHistoryPage;
