0e86d449a45c9c7d19877ab28e8720e9840f22ae
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-14 22:16:10 +0530

    Add and update Watch Page with UI and functionality improvements

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -139,7 +139,24 @@ const WatchPage = () =>{
                     ) }
                 </div>
 
-                
+                <div className="mb-4 md:mb-0">
+                    <h2 className="text-5xl font-bold text-balance">
+                        {content?.title || content?.name}
+                    </h2>
+                    <p className="mt-2 text-lg">
+                        {formatReleaseDate(content?.realease_date || content?.first_air_date)} |{" "}
+                        {content?.adult ? (
+                            <span className="text-red-600">18+</span>
+                        ):(
+                            <span className="text-green-600">PG-13</span>
+                        )}{" "}
+                    </p>
+                    <p className="mt-4 text-lg">{content?.overview}</p>
+                </div>
+                <img
+                    src={ORIGINAL_IMG_BASE_URL + content?.poster_path}
+                    alt="Poster image"
+                    className="max-h-[600px] rounded-md"/>
             </div>
         </div>
     )
