eab1dc778e3752dc6bc770d61bb4e94ac13f0ccd
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-14 19:57:28 +0530

    Add and update Watch Page with UI and functionality improvements

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -8,3 +8,33 @@ import ReactPlayer from "react-player";
 import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constants.js";
 import { formatReleaseDate } from "../utils/dataFunctions.js";
 import WatchPageSkeleton from "../components/WatchPageSkeleton.jsx";
+
+const WatchPage = () =>{
+
+    const { id } = useParams();
+    const [ trailers, setTrailers] = useState([]);
+    const [currentTrailerIdx,setCurrentTrailerIdx] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [content, setContent] = useState({});
+    const [similarContent, setSimilarContent] = useState([]);
+    const { contentType } =useContentStore();
+
+    const sliderRef = useRef(null);
+
+    useEffect(() =>{
+        const getTrailers = async () => {
+            try {
+                const res =await axios.get(`/api/v1/${contentType}/${id}/trailers`);
+                setTrailers(res.data.trailers);
+            }
+            catch(error){
+                if(error.message.includes("404")){
+                    setTrailers([]);
+                }
+            }
+        };
+        getTrailers();
+    }, [contentType, id]);
+}
+
+export default WatchPage;
