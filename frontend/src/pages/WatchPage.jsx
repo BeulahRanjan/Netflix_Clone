b24a5699ae9c1e520811debdfd367e879414755b
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-14 22:07:45 +0530

    Add and update Watch Page with UI and functionality improvements

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -103,6 +103,46 @@ const WatchPage = () =>{
         )
     }
 
+    return(
+        <div className="bg-black min-h-screen text-white">
+            <div className="mx-auto container px-4 py-8 h-full">
+                <Navbar/>
+
+                {trailers.length > 0 && (
+                    <div className="flex justify-between items-center mb-4">
+                        <button className={`
+                        bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                            currentTrailerIdx === 0 ? "opacity-50 cursor-not-allowed":""}
+                        }`}
+                        disabled={currentTrailerIdx === trailers.length -1}
+                        onClick={handleNext}>
+                            <ChevronLeft size={24}/>
+                        </button>
+                    </div>
+                )}
+
+                <div className="aspect-video mb-8 p-2 sm:px-10 md:px-32">
+                    {trailers.length > 0 && (
+                        <ReactPlayer 
+                             controls={true}
+                             width={"100%"}
+                             height={"70vh"}
+                             className="mx-auto overflow-hidden rounded-lg"
+                             url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`}/>
+                    )}
+
+                    {trailers?.length === 0 && (
+                        <h2 className="text-xl text-center mt-5">
+                            No trailers available for{" "}
+                            <span className="font-bold text-red-600">{content?.title || content?.name}</span> 😥
+                        </h2>
+                    ) }
+                </div>
+
+                
+            </div>
+        </div>
+    )
 }
 
 export default WatchPage;
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,11 +1,11 @@
 import AuthScreen from "../home/AuthScreen";
 import {useAuthStore } from "../../store/authUser";
-import WatchPageSkeleton from "../../components/WatchPageSkeleton.jsx";
+import WatchPage from "../WatchPage";
 const HomePage = () => {
     const {user} = useAuthStore();
     return(
         <div>
-            {user ? <AuthScreen /> : <WatchPageSkleteton /> }
+            {user ? <AuthScreen /> : <WatchPage /> }
         </div>
     )
 }
