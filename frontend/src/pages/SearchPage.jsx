a8fc119ecd1511a733f579f3962d0c1c0a516f4c
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-16 22:47:59 +0530

    Add and update Search Page with UI and functionality improvements

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -7,4 +7,36 @@ import axios from "axios";
 import { ORIGINAL_IMG_BASE_URL } from "../utils/constants.jsx";
 import { Link } from "react-router-dom";
 
+const SearchPage = () => {
+    const [activeTab, setActiveTab] = useState("movie");
+    const [searchTerm, setSearchTerm] = useState("");
 
+    const [results, setResults]=useState([]);
+    const {setContentType} = useContentStore();
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+        tab === "movie"? setContentType("movie") : setContentType("tv");
+        setResults([]);
+    };
+
+    const handleSearch = async(e) =>{
+        e.preventDefault();
+        try{
+            const res = await axios.get(`/api/v1/search/${activeTab}/${searchTerm}`);
+            setResults(res.data.content);
+        } catch(error){
+            if(error.response.status === 404){
+                toast.error("Nothing found, make sure you are searching under the right category");
+            } else {
+                toast.error("An error ocurred, please try again later");
+            }
+        }
+    };
+
+    return(
+        
+    )
+}
+
+export default SearchPage;
