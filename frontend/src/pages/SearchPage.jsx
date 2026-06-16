40de263b4175331e0b90f9c47023595691a98743
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-16 23:07:07 +0530

    Add and update Search Page with UI and functionality improvements

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -35,7 +35,34 @@ const SearchPage = () => {
     };
 
     return(
-        
+        <div className="bg-black min-h-screen text-white">
+            <Navbar/>
+            <div className="container mx-auto px-4 py-8">
+                <div className="flex justify-center gap-3 mb-4">
+                    <button
+                        className={`py-2 px-4 rounded ${
+                            aciveTab === "movie" ? "bg-red-600" : "bg-gray-800"
+                        } hover:bg-red-700`}
+                        onClick={() => handleTabClick("movie")}>
+                            Movies
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded ${
+                            activeTab ==="tv"? "bg-red-600" : "bg-gray-800"
+                        } hover:bg-red-700`}
+                        onClick={() => handleTabClick("tv")}>
+                            TV Shows
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded ${
+                            activeTab === "person" ? "bg-red-600" : "bg-gray-800"
+                        } hover:bg-red-700`}>
+                            Person
+                        </button>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
