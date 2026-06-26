78f124830874b144f27ef14d62cd452f0f388bee
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-26 23:52:34 +0530

    Updated Code

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export const ENV_VARS={
-    MONGO_URI:"mongodb://beulahranjan_db_user:Beuheph*77@ac-2icuifb-shard-00-00.sobg7v9.mongodb.net:27017,ac-2icuifb-shard-00-01.sobg7v9.mongodb.net:27017,ac-2icuifb-shard-00-02.sobg7v9.mongodb.net:27017/?ssl=true&replicaSet=atlas-ylftpu-shard-0&authSource=admin&appName=Cluster0",
-    PORT: process.env.PORT || 5000,
-    JWT_SECRET: "your_JWT_SECRET_here",
-    NODE_ENV:"development",
-    TMDB_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjM0NmNhOTI3NTFkN2Y2ODg4NWQwNjc4NWZmNTY2OSIsIm5iZiI6MTc3ODEzMzkyNC4xNzEsInN1YiI6IjY5ZmMyYmE0ZDUyMjU0NjFiY2VkMmMyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AGL7C6quZ0c77wWO9rSH6cLTx3Zb8_5QlSssy6116uY"
-} 
+// export const ENV_VARS={
+//     MONGO_URI:"mongodb://beulahranjan_db_user:Beuheph*77@ac-2icuifb-shard-00-00.sobg7v9.mongodb.net:27017,ac-2icuifb-shard-00-01.sobg7v9.mongodb.net:27017,ac-2icuifb-shard-00-02.sobg7v9.mongodb.net:27017/?ssl=true&replicaSet=atlas-ylftpu-shard-0&authSource=admin&appName=Cluster0",
+//     PORT: process.env.PORT || 5000,
+//     JWT_SECRET: "your_JWT_SECRET_here",
+//     NODE_ENV:"development",
+//     TMDB_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjM0NmNhOTI3NTFkN2Y2ODg4NWQwNjc4NWZmNTY2OSIsIm5iZiI6MTc3ODEzMzkyNC4xNzEsInN1YiI6IjY5ZmMyYmE0ZDUyMjU0NjFiY2VkMmMyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AGL7C6quZ0c77wWO9rSH6cLTx3Zb8_5QlSssy6116uY"
+// } 
--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import tvRoutes from "./routes/tvRoutes.js";
 import searchRoutes from "./routes/searchRoutes.js";
-import { ENV_VARS } from "./config/envVars.js";
+// import { ENV_VARS } from "./config/envVars.js";
  import { connectDB } from "./config/db.js";
  import cookieParser from 'cookie-parser';
  import { protectRoute } from "./middleware/protectRoute.js";
  import path, { dirname } from 'path';
 
  const app= express();
-const PORT = ENV_VARS.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const _dirname=path.resolve();
 
 app.use(express.json());
@@ -34,7 +34,7 @@ app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
 
-if(ENV_VARS.NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(_dirname,"/frontend/dist")));
 
     app.get("*" , (req,res) =>{
