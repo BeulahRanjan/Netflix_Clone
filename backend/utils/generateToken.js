4380d8003ae1257aab31704a4ce1938f5d0a633d
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-26 23:54:43 +0530

    Updated Code

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
-import { ENV_VARS } from '../config/envVars.js';
+// import { ENV_VARS } from '../config/envVars.js';
 
 export const generateTokenAndSetCookie= (userId,res) =>{
-    const token =jwt.sign({userId}, ENV_VARS.JWT_SECRET, {expiresIn :'17d'})
+    const token =jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn :'17d'})
     res.cookie('jwt-netflix', token,{
         maxAge:17*24*60*60*1000,
         ttpOnly:true,
         sameSite:"strict",
-        secure:ENV_VARS.NODE_ENV !=='development',
+        secure:process.env.NODE_ENV !=='development',
     })
     return token;
 };
