ac0fc5433ac5819d86bded228685456bb8ac6a8d
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-29 23:44:49 +0530

    Updated Code

--- backend/services/tmdbService.js
+++ backend/services/tmdbService.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
-// import  { ENV_VARS } from '../config/envVars.js';
+import  { ENV_VARS } from '../config/envVars.js';
 
-// export const fetchFromTMDB =async (url) =>{
-//      const options={
-//         headers:{
-//             accept:'application/json',
-//             Authorization:'Bearer ' + ENV_VARS.TMDB_API_KEY
-//         }
-//      };
+export const fetchFromTMDB =async (url) =>{
+     const options={
+        headers:{
+            accept:'application/json',
+            Authorization:'Bearer ' + ENV_VARS.TMDB_API_KEY
+        }
+     };
  
-   //   const response = await axios.get(url,options);
-   //   if(response.status!==200){
-   //      throw new Error('Failed to fetch data from TMDB');
-   //   }
+     const response = await axios.get(url,options);
+     if(response.status!==200){
+        throw new Error('Failed to fetch data from TMDB');
+     }
 
 export const fetchFromTMDB = async (url, retries = 3) => {
       console.log("TMDB KEY EXISTS:", !!process.env.TMDB_API_KEY);
