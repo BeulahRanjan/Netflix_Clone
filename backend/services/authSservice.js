91574f4d0d3d9ea235874886376dd60a7a76b1f1
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-06-30 23:23:16 +0530

    Updated Code

---
+++ backend/services/authSservice.js
@@ -0,0 +1,16 @@
+export async function signup(body,res){
+     const {email ,password, username} = body;
+
+     const data={
+        email,
+        password,
+        username
+     }
+
+     return await authRepositary.signup(data,res);
+
+
+}
+export async function login{}
+export async function logout{}
+export async function authCheck{}
