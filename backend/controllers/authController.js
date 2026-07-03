b5db38838630e92cbf4cfd2a3dc6a17883f4ef4c
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-07-03 20:58:09 +0530

    Updated Code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,12 @@
 import { User } from "../models/userModel.js";
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
 export async function signup(req,res,next){
     try{
          //const {email ,password, username} = req.body;
 
-         if(!email || !password || !username) {
+         //if(!email || !password || !username) {
         //     console.log(email,password, username);
         //     return res.status(400).json({success:false, message:'All fields are required'});
         // }
