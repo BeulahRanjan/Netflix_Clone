9d4dfa9baad9e5cf000b0252e4f430a748070d76
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-07 22:14:24 +0530

    Updated Code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { signupValidator, loginValidator } from '../helpers/authValidator.js';
 import { signup, login,logout,authCheck } from '../controllers/authController.js';
+import { validate } from '../middleware/validate.js';
 import {protectRoute} from '../middleware/protectRoute.js';
 const router = express.Router();
 
-router.post('/signup',signup);
-router.post('/login',login);
-router.post('/logout',logout);
-router.get('/auth-check',protectRoute,authCheck);
+router.post('/signup', signupValidator,validate, signup);
+router.post('/login', loginValidator,validate, login);
+router.post('/logout', logout);
+router.get('/auth-check', protectRoute, authCheck);
 
 export default router;
