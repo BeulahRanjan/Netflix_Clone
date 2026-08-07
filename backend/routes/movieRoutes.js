1955838f495446fda1a6658216f6823dee017496
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-07 23:51:41 +0530

    Updated Code

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,13 +1,15 @@
   import express from 'express';
   import { getTrendingMovies,getMovieTrailers,getMovieDetails,getMoviesByCategory,getSimilarMovies} from '../controllers/movieController.js';
+  import { trailerValidator,detailsValidator,similarValidator,categoryValidator } from '../helpers/movieValidator.js';
+  import { validate } from '../middlewares/validate.js';
 
   const router= express.Router();
 
   router.get('/trending', getTrendingMovies);
-  router.get('/:id/trailers', getMovieTrailers);
-  router.get('/:id/details', getMovieDetails);
-  router.get('/:id/similar', getSimilarMovies);
-  router.get('/:category', getMoviesByCategory); 
+  router.get('/:id/trailers', validate(trailerValidator), validate, getMovieTrailers);
+  router.get('/:id/details', validate(detailsValidator), validate, getMovieDetails);
+  router.get('/:id/similar', validate(similarValidator), validate, getSimilarMovies);
+  router.get('/:category', validate(categoryValidator), validate, getMoviesByCategory); 
 
   export default router;
  
