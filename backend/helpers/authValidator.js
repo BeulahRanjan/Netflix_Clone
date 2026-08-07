cec60e167e0cf07e4d5c45c0e28e42da679a2bdb
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-07 22:07:17 +0530

    Updated Code

--- backend/helpers/authValidator.js
+++ backend/helpers/authValidator.js
@@ -7,4 +7,14 @@ export const signupValidator = [
     .isEmail().withMessage('Please provide a valid email')
     .isLength({min:6}).withMessage('Email must be at least 6 characters long')
     .isEmail().normalizeEmail(),
+
+
+    body('password')
+    .trim()
+    .notEmpty().withMessage('Password is required')
+    .isLength({min:6}).withMessage('Password must be at least 6 characters long')
+    .matches(/[A-Z]/).withMessage("Password must contain atleast one uppercase character")
+    .matches(/[a-z]/).withMessage("Password must contain atleast one lowercase character")
+    .matches(/[0-9]/).withMessage("Password must contain atleast one digit")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage("Password must contain atleast one special character")
 ]
