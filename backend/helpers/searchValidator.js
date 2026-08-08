487ae138af78cbffee64016f21092dc799657e32
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-08 22:29:35 +0530

    Updated Code

--- backend/helpers/searchValidator.js
+++ backend/helpers/searchValidator.js
@@ -15,3 +15,11 @@ export const searchMovieValidator=[
     .isString().withMessage('Query must be a string')
     .isLength({min:3}).withMessage('Query must be at least 3 characters long')
 ]
+
+export const searchTvValidator=[
+    check('query')
+    .trim()
+    .notEmpty().withMessage('Query is required')
+    .isString().withMessage('Query must be a string')
+    .isLength({min:3}).withMessage('Query must be at least 3 characters long')
+]
