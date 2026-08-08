c42093e8bd5f18bff6e28a517d4c0e5609134e59
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-09 00:02:56 +0530

    Updated Code

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -2,13 +2,14 @@ import express from 'express';
 import { getSearchHistory,removeItemFromSearchHistory,
     searchMovie,searchPerson,searchTv
  } from '../controllers/searchController.js';
+ import { searchPersonValidator, searchMovieValidator,searchTvValidator,removeItemFromSearchHistoryValidator } from '../helpers/searchValidator.js';
 
  const router = express.Router();
 
- router.get("/person/:query", searchPerson);
- router.get("/movie/:query", searchMovie);
- router.get("/tv/:query", searchTv);
+ router.get("/person/:query",searchPersonValidator, validate, searchPerson);
+ router.get("/movie/:query",searchMovieValidator,validate, searchMovie);
+ router.get("/tv/:query", searchTvValidator,validate, searchTv);
  router.get("/history", getSearchHistory);
- router.delete("/history/:id", removeItemFromSearchHistory);
+ router.delete("/history/:id", removeItemFromSearchHistoryValidator,validate, removeItemFromSearchHistory);
 
  export default router; 
