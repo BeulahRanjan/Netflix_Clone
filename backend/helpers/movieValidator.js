49841ceef47e2a0727a43dfa57b9095417b4fb5d
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-08 21:58:57 +0530

    Updated Code

--- backend/helpers/movieValidator.js
+++ backend/helpers/movieValidator.js
@@ -2,25 +2,25 @@ import {body} from 'express-validator';
 import { NOW_PLAYING, TOP_RATED, POPULAR, UPCOMING } from '../utils/constants.js';
  
 export const trailerValidator=[
-    body('id')
+    check('id')
     .notEmpty().withMessage('Movie ID is required') 
     .isInt().withMessage('Movie ID must be an integer')
 ]
 
 export const detailsValidator=[
-    body('id')
+    check('id')
     .notEmpty().withMessage('Movie ID is required') 
     .isInt().withMessage('Movie ID must be an integer')
 ]
 
 export const similarValidator=[
-    body('id')
+    check('id')
     .notEmpty().withMessage('Movie ID is required') 
     .isInt().withMessage('Movie ID must be an integer')
 ]
 
 export const categoryValidator=[
-    body('category')
+    check('category')
     .notEmpty().withMessage('Category is required') 
     .isString().withMessage('Category must be a string')
     .custom((value) =>{
