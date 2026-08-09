4d87ed56d1a16b12ecdb46f7fa86d29d80a5d2d7
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-09 16:32:36 +0530

    Updated Code

--- backend/helpers/tvValidator.js
+++ backend/helpers/tvValidator.js
@@ -1,2 +1,33 @@
 import {check} from 'express-validator';
 
+import { NOW_PLAYING, TOP_RATED, POPULAR, UPCOMING } from '../utils/constants.js';
+ 
+export const trailerValidator=[
+    check('id')
+    .notEmpty().withMessage('Movie ID is required') 
+    .isInt().withMessage('Movie ID must be an integer')
+]
+
+export const detailsValidator=[
+    check('id')
+    .notEmpty().withMessage('Movie ID is required') 
+    .isInt().withMessage('Movie ID must be an integer')
+]
+
+export const similarValidator=[
+    check('id')
+    .notEmpty().withMessage('Movie ID is required') 
+    .isInt().withMessage('Movie ID must be an integer')
+]
+
+export const categoryValidator=[
+    check('category')
+    .notEmpty().withMessage('Category is required') 
+    .isString().withMessage('Category must be a string')
+    .custom((value) =>{
+        if(value == NOW_PLAYING || value == TOP_RATED || value == POPULAR || value == UPCOMING){
+            return true;
+        }
+        throw new Error('Invalid category');
+    })
+]
