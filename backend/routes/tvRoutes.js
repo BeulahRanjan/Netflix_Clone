70ccc7d981bc070b67ccd159a6b05099c7afa861
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-09 16:36:58 +0530

    Updated Code

--- backend/routes/tvRoutes.js
+++ backend/routes/tvRoutes.js
@@ -8,6 +8,6 @@ router.get('/trending', getTrendingTV);
 router.get('/:id/trailers',trailerValidator,validate, getTVTrailers);
 router.get('/:id/details',detailsValidator,validate, getTVDetails);
 router.get('/:id/similar',similarValidator,validate, getSimilarTV);
-router.get('/:category', getTVByCategory );
+router.get('/:category',categoryValidator,validate, getTVByCategory );
 
 export default router; 
