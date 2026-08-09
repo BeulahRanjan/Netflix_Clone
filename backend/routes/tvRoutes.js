2489f7cd0935dcf5e83b368ac2b6873424b981c8
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-09 16:36:39 +0530

    Updated Code

--- backend/routes/tvRoutes.js
+++ backend/routes/tvRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/trending', getTrendingTV);
 router.get('/:id/trailers',trailerValidator,validate, getTVTrailers);
 router.get('/:id/details',detailsValidator,validate, getTVDetails);
-router.get('/:id/similar', getSimilarTV);
+router.get('/:id/similar',similarValidator,validate, getSimilarTV);
 router.get('/:category', getTVByCategory );
 
 export default router; 
