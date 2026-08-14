2c5f03813649fbbb2cd00c6c353e9d3e4e8b8e18
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-08-14 21:30:24 +0530

    Updated Code

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -24,11 +24,11 @@ const MovieSlider =({category})=>{
             console.log("URL:", `/api/v1/${contentType}/${category}`);
             console.log("SLIDER RESPONSE:", res.data);
             console.log("SLIDER CONTENT:", res.data.content);
-            console.log(
-                "IS ARRAY:",
-                Array.isArray(res.data.content)
-            );
-            console.log("================================");
+            // console.log(
+            //     "IS ARRAY:",
+            //     Array.isArray(res.data.content)
+            // );
+            // console.log("================================");
             setContent(res.data.content);
         };
         getContent();
