e3ad4c63d0681de32e3bf3d79571c0b554388b94
Beulah Ranjan <beulahranjan@gmail.com>
2026-09-03 22:54:10 +0530

    Added a Rate Limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,4 +1,7 @@
 import redisClient from "../config/redis.js";
 export async function rateLimiter(req,res,next){
+    const ip= req.ip;
+    const key=`rate_limit:${ip}`;
+    const bucket= await redisClient.get(key);
 
 }
