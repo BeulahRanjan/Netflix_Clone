43bcce2f55d9a47cf55b521004af7dbef54814db
Beulah Ranjan <beulahranjan@gmail.com>
2026-09-04 21:58:58 +0530

    Added a Rate Limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -11,7 +11,7 @@ export async function rateLimiter(req,res,next){
     if(exists){
     const bucket= JSON.parse(await redisClient.get(key));
     const now= Date.now();
-    const elaspedTime= now - bucket.lastRefillTime;
+    const elapsedTime= now - bucket.lastRefillTime;
     const elapsedSeconds=elapsedTime/1000;
 
     }else{
