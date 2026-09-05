1a96e796f07f6755ad5b0a54a135195689355007
Beulah Ranjan <beulahranjan@gmail.com>
2026-09-05 17:58:37 +0530

    Added a Rate Limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -11,12 +11,23 @@ export async function rateLimiter(req,res,next){
     if(exists){
     const bucket= JSON.parse(await redisClient.get(key));
     const now= Date.now();
-    const REFILL_RATE =5;
+    //const REFILL_RATE =5;
     const elapsedTime= now - bucket.lastRefillTime;
     const elapsedSeconds=elapsedTime/1000;
     const tokensToAdd= elapsedSeconds * REFILL_RATE;
     bucket.tokens= Math.min(bucket.tokens + tokensToAdd, CAPACITY);
     bucket.lastRefillTime= now;
+
+    if(bucket.tokens>=1){
+        bucket.tokens--;
+        await redisClient.set(key, JSON.stringify(bucket));
+        return next();
+    }
+
+    return res.status(429).json({
+        success:false,
+        message:"Too many requests"
+    })
 
     }else{
 
@@ -28,5 +39,7 @@ export async function rateLimiter(req,res,next){
         bucket.tokens--;
 
         await redisClient.set(key, JSON.stringify(bucket));
+
+        return next();
     }
 }
