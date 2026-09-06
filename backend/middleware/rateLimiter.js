93f455cf5824b9ff8772909ea8c601588a417481
Beulah Ranjan <beulahranjan@gmail.com>
2026-09-06 19:05:12 +0530

    Added a Rate Limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -4,7 +4,7 @@ import redisClient from "../config/redis.js";
     const REFILL_RATE =0;
 export async function rateLimiter(req, res, next) {
 
-    console.log("RATE LIMITER HIT");
+    //console.log("RATE LIMITER HIT");
 
     const ip = req.ip === "::1" || req.ip === "::ffff:127.0.0.1"
         ? "127.0.0.1"
@@ -34,11 +34,11 @@ export async function rateLimiter(req, res, next) {
     const elapsedSeconds=elapsedTime/1000;
     const tokensToAdd= elapsedSeconds * REFILL_RATE;
     console.log("NOW:", now);
-console.log("LAST REFILL:", bucket.lastRefillTime);
-console.log("ELAPSED MS:", elapsedTime);
-console.log("ELAPSED SEC:", elapsedSeconds);
-console.log("TOKENS TO ADD:", tokensToAdd);
-console.log("TOKENS BEFORE:", bucket.tokens);
+    console.log("LAST REFILL:", bucket.lastRefillTime);
+    console.log("ELAPSED MS:", elapsedTime);
+    console.log("ELAPSED SEC:", elapsedSeconds);
+    console.log("TOKENS TO ADD:", tokensToAdd);
+    console.log("TOKENS BEFORE:", bucket.tokens);
     bucket.tokens= Math.min(bucket.tokens + tokensToAdd, CAPACITY);
     console.log("TOKENS AFTER REFILL:", bucket.tokens);
     bucket.lastRefillTime= now;
