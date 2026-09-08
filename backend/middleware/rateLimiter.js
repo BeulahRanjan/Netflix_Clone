91d759eb9cecf7fdac073a2bc544cfdb55d4fc83
Beulah Ranjan <beulahranjan@gmail.com>
2026-09-08 22:28:35 +0530

    Added a Rate Limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -38,8 +38,8 @@ export async function rateLimiter(req, res, next) {
     console.log("LAST REFILL:", bucket.lastRefillTime);
     console.log("ELAPSED MS:", elapsedTime);
     console.log("ELAPSED SEC:", elapsedSeconds);
-    //console.log("TOKENS TO ADD:", tokensToAdd);
-    console.log("TOKENS BEFORE:", bucket.tokens);
+    console.log("TOKENS TO ADD:", tokensToAdd);
+    //console.log("TOKENS BEFORE:", bucket.tokens);
     bucket.tokens= Math.min(bucket.tokens + tokensToAdd, CAPACITY);
     console.log("TOKENS AFTER REFILL:", bucket.tokens);
     bucket.lastRefillTime= now;
